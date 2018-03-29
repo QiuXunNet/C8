93246329b534d1ccdbbb7fc2d62b3dae1c684b31
luchen <462326994@qq.com>
2018-03-29 12:06:15 +0800

    创建聊天控制器

---
+++ C8.Lottery.Portal/C8.Lottery.Portal/Controllers/TalkingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace C8.Lottery.Portal.Controllers
+{
+    public class TalkingController : BaseController
+    {
+        //
+        // GET: /Talking/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
