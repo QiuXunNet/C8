b976f81a59b8ac1f8fd8ce13fdbb674fc8581847
KCP <2577168006@qq.com>
2018-04-24 10:59:09 +0800

    bug ico图标

--- C8.Lottery.Portal/C8.Lottery.Portal/js/yjp.js
+++ C8.Lottery.Portal/C8.Lottery.Portal/js/yjp.js
@@ -60,15 +60,12 @@ $(function () {
         $(this).addClass("current").siblings().removeClass("current");
         var zhi = $(this).index();
         $(".hjc_indexmain .ind_mainUl2").eq(zhi).show().siblings().hide();
-    });
-
-    /*index-切换彩种*/
-    $(".hdNav_cai li").click(function () {
-        $(this).addClass("current").siblings().removeClass("current");
-        var zhi = $(this).index();
-        $(".hjc_indexmain .ind_mainUl2").eq(zhi).show().siblings().hide();
 
         $(".hdNav_cai_collect .hdNav_cai_lhs li").eq(zhi).addClass("current").siblings().removeClass("current");
+		
+		$(".hdNav_cai_expand").hide();
+		$(".mask").hide();
+		
     });
 
     /*** Added By LHS 2018-3-1  */
@@ -82,6 +79,10 @@ $(function () {
         //展开项添加选中
         $(".hdNav_cai_expand .hdNav_cai li").eq(zhi).addClass("current").siblings().removeClass("current");
         $(".GS_box .GS_nav").eq(zhi).show().siblings().hide();
+		
+		$(".hdNav_cai_expand").hide();
+		$(".mask").hide();
+		
     });
 
     /* 展开项点击 */
@@ -117,9 +118,9 @@ $(function () {
     });
 
     $(".hdNav_cai_collect .hdNav_cai_lhs_expand").click(function () {
-        $(".hdNav_cai_collect").hide();
-        $(".hdNav_cai_expand").show();
-        $(".mask").show();
+        //$(".hdNav_cai_collect").hide();
+        $(".hdNav_cai_expand").toggle();
+        $(".mask").toggle();
     });
 
     $(".hdNav_cai_expand .collect").click(function () {
@@ -406,6 +407,7 @@ $(function () {
                 ctype: ctype,
                 type: type
             }, function (result) {
+                console.log(result);
                 if (result) {
                     if (result.Code === 401) {
                         location.href = "/Home/Login";
