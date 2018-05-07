aefdde3dd167792b0ee9115f891eeeff5281da82
luohongshun <453761305@qq.com>
2018-05-07 09:51:01 +0800

    六彩开奖时间页添加去header参数

--- C8.Lottery.Portal/C8.Lottery.Portal/js/baidu-statistics.js
+++ C8.Lottery.Portal/C8.Lottery.Portal/js/baidu-statistics.js
@@ -1,7 +1,7 @@
 ﻿var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?8a9cd995b528b4a0a0630e0b65c07621";
+    hm.src = "https://hm.baidu.com/hm.js?183c3718987e9091be04aa7ecdab4420";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
