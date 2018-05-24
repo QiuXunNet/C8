cdfc32c25319e780c1a6a627787944869a4617c2
luchen <462326994@qq.com>
2018-05-24 15:59:37 +0800

    tijiao

--- C8.Lottery.Portal/C8.Lottery.Portal/Controllers/InController.cs
+++ C8.Lottery.Portal/C8.Lottery.Portal/Controllers/InController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using C8.Lottery.Model;
@@ -25,50 +26,41 @@ namespace C8.Lottery.Portal.Controllers
 
             if (bo == "true")
             {
-                var friendLinkList =
-               Util.ReaderToList<FriendLink>("select * from dbo.FriendLink where [Type]=1 and state = 0 and Code=@Code",
-                   new SqlParameter("@Code", id));
+                List<FriendLink> list = CacheHelper.GetCache<List<FriendLink>>("FriendshipLinksControllerFriendLinkList");
 
-                if (friendLinkList != null && friendLinkList.Count > 0)
+                if (list == null)
+                {
+                    list = Util.ReaderToList<FriendLink>("select * from dbo.FriendLink where [Type]=1 and state = 0 ");
+
+                    CacheHelper.SetCache("FriendshipLinksControllerFriendLinkList", list, DateTime.Now.AddHours(2));
+                }
+
+                var friendLink = list.FirstOrDefault();
+
+                if (friendLink != null)
                 {
                     Session["LinkCode"] = id;
 
-                    var friendLink = friendLinkList.FirstOrDefault();
                     url = friendLink.TransferUrl.ToLower();
 
                     //判断是否存在http://
-                    if (url.IndexOf("http://") != 0)
+                    if (!(url.IndexOf("http://") > -1 || url.IndexOf("https://") > -1))
                     {
-                        url = "http://" + url;
+                        url = "https://" + url;
                     }
 
-                    string visitRecordSql = "select count(*) from dbo.LinkVisitRecord where RefId=@RefId and SubTime=@Date and [Type]=1";
-                    //添加
-                    var sqlParameter = new[]
+                    var obj = CacheHelper.GetCache<int>("FriendshipLinksControllerLink" + id);
+                    if (obj == default(int))
                     {
-                        new SqlParameter("@RefId",friendLink.Id),
-                        new SqlParameter("@Date",DateTime.Today),
-                };
-                    var count = Convert.ToInt32(SqlHelper.ExecuteScalar(visitRecordSql, sqlParameter));
-
-                    if (count == 0)
-                    {
-                        //新增
-                        string insertRecordSql = string.Format(
-                                @"insert into dbo.LinkVisitRecord (RefId,ClickCount,UV,IP,PV,RegCount,Type,SubTime)
-                        values({0},1,0,0,0,0,1,'{1}')", friendLink.Id, DateTime.Today);
-                        SqlHelper.ExecuteScalar(insertRecordSql);
-
+                        CacheHelper.AddCache("FriendshipLinksControllerLink" + id, 1);
                     }
                     else
                     {
-                        //修改
-                        string updateRecordSql = string.Format(@"update dbo.LinkVisitRecord set ClickCount+=1 where RefId={0} and SubTime = '{1}' and [Type]=1 ", friendLink.Id, DateTime.Today);
-                        SqlHelper.ExecuteScalar(updateRecordSql);
+                        var uvNum = Convert.ToInt32(CacheHelper.GetCache<int>("FriendshipLinksControllerLink" + id));
+                        CacheHelper.SetCache("FriendshipLinksControllerLink" + id, uvNum + 1);
                     }
                 }
             }
-
             Response.Redirect(url);
         }
     }
